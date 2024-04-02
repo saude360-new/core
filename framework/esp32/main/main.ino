cc297802d43a6ec5e3a455ce6308d258ca36c5d1
gitlab-az1 <gitlab.az1@gmail.com>
2024-04-02 15:01:38 -0300

    feat: initial environment setup

---
+++ framework/esp32/main/main.ino
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+
+void setup() {
+  // put your setup code that will be executed once here:
+
+}
+
+
+void loop() {
+  // put your code that will be executed in loop here:
+
+}
