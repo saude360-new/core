0cff46430e4919a7c20643a0ab86c035ab721456
saude360-new <health-care.external-services-gateway.si_admin.sio@w4io.com>
2025-10-15 08:46:12 -0300

    Update fall.h

--- firmware/esp32/main/parts/fall.h
+++ firmware/esp32/main/parts/fall.h
@@ -1,6 +1,6 @@
-#pragma once
+#ifndef FALLDETECTOR_H
+#define FALLDETECTOR_H
 
-#include <Arduino.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 
@@ -16,3 +16,30 @@ typedef struct {
   float gy;
   float gz;
 } MPUPayload;
+
+class FallDetector {
+public:
+  FallDetector();
+  void process_sensor_data(const MPUPayload& data);
+  bool is_fall();
+
+private:
+  float FREE_FALL_THRESHOLD;
+  float IMPACT_THRESHOLD;
+  float INACTIVITY_THRESHOLD;
+
+  bool fall_detected;
+  bool in_free_fall;
+  bool impact_occured;
+
+  static const int RECENT_DATA_SIZE = 50;
+  MPUPayload recent_data[RECENT_DATA_SIZE];
+  int d_index;
+
+  float calculate_va(const MPUPayload& data) const;
+  float calculate_gs(const MPUPayload& data) const;
+  float calculate_sa() const;
+  void update_recent_data(const MPUPayload& data);
+};
+
+#endif
